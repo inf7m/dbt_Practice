88bb08b28cdaab8165d69ec29e0373ebe1d3bc71
truongdang.learning@gmail.com <truongdang.learning@gmail.com>
2023-07-05 14:27:17 +0000

    UPDATE

--- models/model1.sql
+++ models/model1.sql
@@ -1,7 +1,6 @@
 WITH TRANS_POPULARITY AS (
     SELECT artist_name, popularity, genre, AVG(loudness) AS avg_loudness
     FROM default.sp1
-    GROUP BY artist_name, popularity, genre
+    GROUP BY artist_name, popularity, genre, loudness
 )
-
-SELECT * FROM TRANS_POPULARITY LIMIT 5;
+SELECT * FROM TRANS_POPULARITY LIMIT 5
